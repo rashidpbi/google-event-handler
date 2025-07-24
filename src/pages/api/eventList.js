0df9eb97a37da65a1dc1cf9292fa06fefa206dfa
Rashid <rashidpbi786@gmail.com>
2025-07-24 22:23:15 +0530

    feat: refresh token mechanism

--- src/pages/api/eventList.js
+++ src/pages/api/eventList.js
@@ -2,14 +2,28 @@ import { google } from "googleapis";
 import oauth2Client from "@/utils/google-auth";
 
 export default async function handler(req, res) {
-  const token = req.cookies.google_access_token;
+   const {
+    google_access_token,
+    expiry_date,
+    refresh_token_expires_in,
+    refresh_token,
+  } = req.cookies;
   if (req.method === "GET") {
     try {
-      if(!token){
+      if(!google_access_token){
         res.status(401).json({error:'Unauthorised - Missing token'})
       }
 
-      oauth2Client.setCredentials({ access_token: token });
+      oauth2Client.setCredentials({
+        access_token: google_access_token,
+        expiry_date,
+        refresh_token_expires_in,
+        refresh_token,
+      });
+      // oauth2Client.setCredentials({ access_token: google_access_token});
+      oauth2Client.on("tokens", (newTokens) => {
+        console.log("🔄 Refreshed tokens:", newTokens);
+      });
       const calendar = google.calendar({ version: "v3", auth: oauth2Client });
   
       const {data} = await calendar.events.list(
